8fb20f9f36189678b48983b24b01fde8d870c503
Ramon <ramonrodsouza@gmail.com>
2025-06-08 14:48:08 -0300

    feature-052 - feat: criando Classe para escala de servico

---
+++ libs/domain/src/lib/ServiceSchedule/ServiceSchedule.ts
@@ -0,0 +1,48 @@
+import { Departament, TimePeriod, WeekDays } from "@domain/enums";
+import { ChildSummary, MemberSummary } from "@domain/user";
+import { v4 as uuidv4 } from 'uuid';
+
+export class ServiceSchedule {
+    constructor(
+        public readonly id: string = uuidv4(),
+        public departament: Departament | null = null,
+        public date: Date = new Date(),
+        public weekDay: WeekDays | null = null,
+        public timePeriod: TimePeriod = TimePeriod.EVENING,
+        public leader: MemberSummary | null = null,
+        public members: MemberSummary[] = [],
+        public childrens: ChildSummary[] = [],
+        public isActive: boolean = true,
+        public createdAt: string = new Date().toISOString() 
+    ) {}
+
+    static fromJson(json: any): ServiceSchedule {
+        return new ServiceSchedule(
+            json.id,
+            json.departament,
+            new Date(json.date),
+            json.weekDay,
+            json.timePeriod,
+            json.leader,
+            json.members || [],
+            json.childrens || [],
+            json.isActive,
+            json.createdAt
+        );
+    }
+
+    toJSON(): object {
+        return {
+            id: this.id,
+            departament: this.departament,
+            date: this.date.toISOString(),
+            weekDay: this.weekDay,
+            timePeriod: this.timePeriod,
+            leader: this.leader,
+            members: this.members, 
+            childrens: this.childrens, 
+            isActive: this.isActive,
+            createdAt: this.createdAt,
+        };
+    }
+} 
