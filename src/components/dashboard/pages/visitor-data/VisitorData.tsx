537e884720d5208f842bb7ef131eff86a948d5b1
Ramon <ramonrodsouza@gmail.com>
2025-06-14 17:19:34 -0300

    refactor-062B - feat: colocando filtro para listar os visitantes ativos

--- src/components/dashboard/pages/visitor-data/VisitorData.tsx
+++ src/components/dashboard/pages/visitor-data/VisitorData.tsx
@@ -2,12 +2,12 @@ import { Info } from "@mui/icons-material";
 import {
     IconButton,
     Typography,
-    Table, 
-    TableBody, 
-    TableCell, 
-    TableContainer, 
-    TableHead, 
-    TableRow, 
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
     Paper,
 } from "@mui/material";
 import { useContext, useEffect, useState } from 'react';
@@ -26,13 +26,13 @@ export default function VisitorData() {
     const [openData, setOpenData] = useState(false);
     const [selectedVisitor, setSelectedVisitor] = useState<Visitor | null>(null);
     const { isMobile } = useContext(ManagerContext);
-    
+
     function handleOpenDetails(visitor: Visitor) {
         setSelectedVisitor(visitor);
         setOpenData(true);
     }
 
-    useEffect(() => { 
+    useEffect(() => {
         findAllVisitors()
             .then((it) => {
                 setData(it);
@@ -51,10 +51,10 @@ export default function VisitorData() {
     }
 
     return (
-        <Layout total={data?.length} title="Visitantes" path="new-visitor" message="Visitante criado com sucesso!">
-            <Search<Visitor> 
-                data={data} 
-                onFilter={setFiltered} 
+        <Layout total={data.filter((it => it.isActive)).length} title="Visitantes" path="new-visitor" message="Visitante criado com sucesso!">
+            <Search<Visitor>
+                data={data}
+                onFilter={setFiltered}
                 label={'Buscar Visitante'}
                 searchBy={(item, term) =>
                     item.name.toLowerCase().includes(term.toLowerCase()) ||
@@ -63,52 +63,52 @@ export default function VisitorData() {
             />
             {filtered?.length > 0 ? (
                 <TableContainer component={Paper}>
-                <Table size="small">
-                    <TableHead>
-                    <TableRow>
-                        <TableCell className='title-secondary'>Nome</TableCell>
-                        <TableCell className='title-secondary'>Telefone</TableCell>
+                    <Table size="small">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell className='title-secondary'>Nome</TableCell>
+                                <TableCell className='title-secondary'>Telefone</TableCell>
 
-                        {!isMobile && (
-                            <>
-                                <TableCell className='title-secondary'>Visitas</TableCell>
-                            </>
-                        )}
-
-                        <TableCell className='title-secondary'>Info</TableCell>
-                    </TableRow>
-                    </TableHead>
-                    <TableBody>
-                    {filtered
-                        .filter((it) => it.isActive)
-                        .sort((a, b) => organizedToLastDate(a, b) )
-                        .map((it) => (
-                            <TableRow key={it.id}>
-                                <TableCell className='data-text'>{it.name}</TableCell>
-                                <TableCell 
-                                    className='data-text onClick' 
-                                    onClick={() => whatzapp(it.name, it.phone)}
-                                >
-                                    {it.phone}
-                                </TableCell>
                                 {!isMobile && (
                                     <>
-                                        <TableCell className='data-text'>{it.visitHistory.at(0)}</TableCell>
+                                        <TableCell className='title-secondary'>Visitas</TableCell>
                                     </>
                                 )}
-                                <TableCell className='data-text'>                                   
-                                    <IconButton onClick={() => handleOpenDetails(it)}>
-                                        <Info/>
-                                    </IconButton> 
-                                </TableCell>
+
+                                <TableCell className='title-secondary'>Info</TableCell>
                             </TableRow>
-                    ))}
-                    </TableBody>
-                </Table>
+                        </TableHead>
+                        <TableBody>
+                            {filtered
+                                .filter((it) => it.isActive)
+                                .sort((a, b) => organizedToLastDate(a, b))
+                                .map((it) => (
+                                    <TableRow key={it.id}>
+                                        <TableCell className='data-text'>{it.name}</TableCell>
+                                        <TableCell
+                                            className='data-text onClick'
+                                            onClick={() => whatzapp(it.name, it.phone)}
+                                        >
+                                            {it.phone}
+                                        </TableCell>
+                                        {!isMobile && (
+                                            <>
+                                                <TableCell className='data-text'>{it.visitHistory.at(0)}</TableCell>
+                                            </>
+                                        )}
+                                        <TableCell className='data-text'>
+                                            <IconButton onClick={() => handleOpenDetails(it)}>
+                                                <Info />
+                                            </IconButton>
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
+                        </TableBody>
+                    </Table>
                 </TableContainer>
             ) : (
                 <Typography variant="body1" sx={{ color: 'var(--primary-title)' }}>
-                Nenhum visitante encontrado.
+                    Nenhum visitante encontrado.
                 </Typography>
             )}
             <VisitorDataModal
