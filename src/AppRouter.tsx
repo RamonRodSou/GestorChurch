a9e58d0b103a9e7b802f49f29a9aa47d1d8304f6
Ramon <ramonrodsouza@gmail.com>
2025-06-22 16:24:58 -0300

    feature-065 - Versao01-IAF

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -23,7 +23,7 @@ import ReportGroupDetails from '@components/dashboard/pages/report-group-data/re
 import { useEffect, useState } from 'react';
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from '@service/firebase';
-import { User } from 'firebase/auth'; 
+import { User } from 'firebase/auth';
 import UserData from '@components/dashboard/pages/userData/UserData';
 import UserDetails from '@components/dashboard/pages/userData/user-details/UserDetails';
 import UserInvited from '@components/dashboard/pages/userData/user-invited/UserInvited';
@@ -43,46 +43,46 @@ function AppRouter() {
         return () => unsubscribe();
     }, []);
 
-  if (loading) return <p>Carregando...</p>; 
-    return (  
-        <BrowserRouter>
+    if (loading) return <p>Carregando...</p>;
+    return (
+        <BrowserRouter basename="/gestor">
             <Routes>
-                    <Route path="new-user" element={<UserDetails/>}/>
+                <Route path="new-user" element={<UserDetails />} />
                 {!user ? (
                     <>
-                        <Route path="/login" element={<Login/>}/>
+                        <Route path="/login" element={<Login />} />
                         <Route path="*" element={<Navigate to="/login" replace />} />
                     </>
-                ) : ( 
-                    <> 
-                        <Route path="/dashboard/:userId" element={<ProtectedRoute><Dashboard/></ProtectedRoute>}>
-                            <Route path="home" element={<Home/>}/>
-                            <Route path="user" element={<UserData/>}/>
-                            <Route path="user/invited" element={<UserInvited/>}/>
-                            <Route path="new-user" element={<UserDetails/>}/>
-                            <Route path="visitor" element={<VisitorData/>}/>
-                            <Route path="new-visitor" element={<VisitorDetails/>}/>
-                            <Route path="/dashboard/:userId/edit-visitor/:visitorId" element={<VisitorDetails/>}/>
-                            <Route path="member" element={<MemberData/>}/>
-                            <Route path="new-member" element={<MemberDetails/>}/>
-                            <Route path="/dashboard/:userId/edit-member/:memberId" element={<MemberDetails/>}/>
-                            <Route path="children" element={<ChildrenData/>}/>
-                            <Route path="new-children" element={<ChildrenDetails/>}/>
-                            <Route path="/dashboard/:userId/edit-children/:childId" element={<ChildrenDetails/>}/>
-                            <Route path="service-schedule" element={<ServiceScheduleData/>}/>
-                            <Route path="new-service-schedule" element={<ServiceScheduleDetails/>}/>
-                            <Route path="group" element={<GroupData/>}/>
-                            <Route path="new-group" element={<GroupDetails/>}/>
-                            <Route path="/dashboard/:userId/edit-group/:groupId" element={<GroupDetails/>}/>
-                            <Route path="financial" element={<FinancialData/>}/>
-                            <Route path="preferences" element={<Preferences/>}/>
-                            <Route path="report" element={<ReportData/>}/>
-                            <Route path="new-report-church" element={<ReportChurchDetails/>}/>
-                            <Route path="report-group" element={<ReportGroupData/>}/>
-                            <Route path="new-report-group" element={<ReportGroupDetails/>}/>
-                            <Route path="visitor-group" element={<VisitorGroupData/>}/>
-                            <Route path="new-visitor-group" element={<VisitorGroupDetails/>}/>
-                            <Route path="/dashboard/:userId/edit-visitor-group/:visitorGroupId" element={<VisitorGroupDetails/>}/>
+                ) : (
+                    <>
+                        <Route path="/dashboard/:userId" element={<ProtectedRoute><Dashboard /></ProtectedRoute>}>
+                            <Route path="home" element={<Home />} />
+                            <Route path="user" element={<UserData />} />
+                            <Route path="user/invited" element={<UserInvited />} />
+                            <Route path="new-user" element={<UserDetails />} />
+                            <Route path="visitor" element={<VisitorData />} />
+                            <Route path="new-visitor" element={<VisitorDetails />} />
+                            <Route path="/dashboard/:userId/edit-visitor/:visitorId" element={<VisitorDetails />} />
+                            <Route path="member" element={<MemberData />} />
+                            <Route path="new-member" element={<MemberDetails />} />
+                            <Route path="/dashboard/:userId/edit-member/:memberId" element={<MemberDetails />} />
+                            <Route path="children" element={<ChildrenData />} />
+                            <Route path="new-children" element={<ChildrenDetails />} />
+                            <Route path="/dashboard/:userId/edit-children/:childId" element={<ChildrenDetails />} />
+                            <Route path="service-schedule" element={<ServiceScheduleData />} />
+                            <Route path="new-service-schedule" element={<ServiceScheduleDetails />} />
+                            <Route path="group" element={<GroupData />} />
+                            <Route path="new-group" element={<GroupDetails />} />
+                            <Route path="/dashboard/:userId/edit-group/:groupId" element={<GroupDetails />} />
+                            <Route path="financial" element={<FinancialData />} />
+                            <Route path="preferences" element={<Preferences />} />
+                            <Route path="report" element={<ReportData />} />
+                            <Route path="new-report-church" element={<ReportChurchDetails />} />
+                            <Route path="report-group" element={<ReportGroupData />} />
+                            <Route path="new-report-group" element={<ReportGroupDetails />} />
+                            <Route path="visitor-group" element={<VisitorGroupData />} />
+                            <Route path="new-visitor-group" element={<VisitorGroupDetails />} />
+                            <Route path="/dashboard/:userId/edit-visitor-group/:visitorGroupId" element={<VisitorGroupDetails />} />
                         </Route>
                         <Route path="*" element={<Navigate to={`/dashboard/${user?.uid}/home`} replace />} />
                     </>
