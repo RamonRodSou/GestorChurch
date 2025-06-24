127cb9991cc94b7c1e0a3d4564315b0f8d264dd6
Ramon <ramonrodsouza@gmail.com>
2025-06-24 14:37:24 -0300

    feature-073 - criando const de msg whatsapp para aniversariante e const de numero por paginas

--- libs/domain/src/lib/utils/string-utils.ts
+++ libs/domain/src/lib/utils/string-utils.ts
@@ -1,6 +1,27 @@
 export const EMPTY: string = '';
+export const rowsPerPage = 30;
+
 export const NOT_REGISTER: string = 'NÃO REGISTRADO';
+
 export const whatAppMessageMember = `como você está? Estamos sentindo sua falta. Você é fundamental para a nossa igreja e faz toda a diferença entre nós.`;
 export const whatAppMessageVisitor = `como você está? Venha nos fazer uma visita, temos cultos: Domingo de manhã às 10:00, e a noite às 18:30, nas quartas-feiras às 19:30. Que Deus abençoe sua vida!`;
 export const whatAppMessageChild = `como você está? Estamos sentindo sua falta.`;
 export const whatAppMessageSupport = `poderia me ajudar? Estou com problemas no Admin Church.`;
+export const whatAppMessageBirthday = `a paz do Senhor!
+Neste dia tão especial, toda a família da Igreja IAF se alegra e celebra com gratidão o grande presente que é a sua vida. 
+
+Saiba que nós amamos ter você em nossa comunidade e desejamos tudo de bom e do melhor que o Pai tem para oferecer.
+
+Nossa oração por você hoje é a que está na Palavra de Deus, em Números 6:24-26:
+
+"O Senhor te abençoe e te guarde;
+o Senhor faça resplandecer o seu rosto sobre ti e tenha misericórdia de ti;
+o Senhor sobre ti levante o seu rosto e te dê a paz."
+
+Que este novo ciclo seja de muita saúde, alegria e intimidade com Deus. 
+
+Feliz aniversário! Receba o nosso abraço mais forte.
+
+Com todo o carinho,
+Igreja IAF!
+`;
