63018f39c10b69963c003154daaa93f8f97e2099
Ramon <ramonrodsouza@gmail.com>
2025-09-09 11:01:10 -0300

    feature-099 - refactor:  formatando as classes, tirando console,  e criando ticketservice, counterService e formatando css

--- libs/domain/src/lib/guest/Guest.ts
+++ libs/domain/src/lib/guest/Guest.ts
@@ -2,16 +2,15 @@ import { EMPTY } from '@domain/utils';
 import { v4 as uuidv4 } from 'uuid';
 
 export class Guest {
-    date: any;
     constructor(
         public readonly id: string = uuidv4(),
         public name: string = EMPTY,
         public phone: string = EMPTY,
         public birthdate: Date | null = null,
         public lotId: string = EMPTY,
+        public ticketNumber: number = 0,
         public isActive: boolean = true,
         public createdAt: string = new Date().toISOString(),
-
     ) { }
 
     static fromJson(json: any): Guest {
@@ -21,8 +20,9 @@ export class Guest {
             json.phone,
             new Date(json.birthdate),
             json.lotId,
+            json.ticketNumber,
             json.isActive,
-            json.createdAt
+            json.createdAt,
         );
     }
 
@@ -33,6 +33,7 @@ export class Guest {
             phone: this.phone,
             birthdate: this.birthdate ? this.birthdate.toISOString() : null,
             lot: this.lotId,
+            ticketNumber: this.ticketNumber,
             isActive: this.isActive,
             createdAt: this.createdAt
         };
